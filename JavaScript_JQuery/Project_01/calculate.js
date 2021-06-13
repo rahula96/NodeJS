460f03c958e1f63f8464db05a8bbf8ead2af047f
rahula96 <rahula96@gmail.com>
2021-06-13 00:21:18 +0000

    Javascript Jquery

---
+++ JavaScript_JQuery/Project_01/calculate.js
@@ -0,0 +1,16 @@
+
+function number_click(params) {
+    alert(params);
+}
+
+function ac_click(params) {
+    alert(params);
+}
+
+function result(params) {
+    alert(params);
+}
+
+function calcu(params) {
+    alert(params);
+}
---
+++ JavaScript_JQuery/Project_02/calculate.js
@@ -0,0 +1,80 @@
+
+let numbers = {};
+const number_max_size = 10;
+
+let numberObj = {
+    'number_before': 0,
+    'operator': '',
+    'number_after': 0,
+    'result': 0,
+};
+
+for (let i = 0; i < number_max_size; i++) {
+    numbers[i] = document.getElementById('number_' + i);
+    numbers[i].addEventListener('click', number_btn_listener);
+    console.log(numbers[i]);
+}
+
+numbers[number_max_size] = document.getElementById('number_dot');
+numbers[number_max_size].addEventListener('click', number_btn_listener);
+console.log(numbers[number_max_size]);
+
+function number_btn_listener(event) {
+    // console.log(event.target.id);
+    // console.log(event.target.innerText);
+
+    number_click(event.target.innerText);
+}
+
+function number_click(params) {
+    // alert(params);
+    let express = document.getElementById('express').firstChild;
+
+    express.appendData(params);
+}
+
+function ac_click(params) {
+    // alert(params);
+    document.getElementById('express').innerHTML = " ";
+    document.getElementById('result').innerHTML = " ";
+
+}
+
+function result(params) {
+
+    let result = 0;
+    let express = document.getElementById('express').innerHTML;
+    const strexpress = express.split(numberObj.operator);
+
+    numberObj.number_before = strexpress[0] === null ? 0 : strexpress[0];
+    numberObj.number_after = strexpress[1] === null ? 0 : strexpress[1];
+
+    switch (numberObj.operator) {
+        case '+':
+            result = numberObj.number_before + numberObj.number_after;
+            break;
+        case '-':
+            result = numberObj.number_before - numberObj.number_after;
+            break;
+        case '/':
+            result = numberObj.number_before / numberObj.number_after;
+            break;
+        case '*':
+            result = numberObj.number_before * numberObj.number_after;
+            break;
+        default:
+            break;
+    }
+
+    document.getElementById('result').innerHTML = result;
+
+}
+
+function calcu(params) {
+    // alert(params);
+    let express = document.getElementById('express').firstChild;
+
+    numberObj.operator = params;
+
+    express.appendData(params);
+}
